df12585bab866b2b88d4c617d78b6f030340681d
technicolour24k <hitetsu@hotmail.com>
2025-01-02 17:44:23 +0000

    Updated framework config
    - Updated bootstrap to define default settings
    - Added user defined settings and config
    - Updated logging in-line documentation

--- utils/logging.lua
+++ utils/logging.lua
@@ -16,7 +16,6 @@ tlp.logging = tlp.logging or {}
 -- tlp.logging.info("Ready to kick Shadow Lords butt!", 5) -- Displays "[Info] Ready to kick Shadow Lords butt!" in colour 5.
 -- tlp.logging.info("Default colour message") -- Displays "[Info] Default colour message" in colour 2.
 -------------------------------------------------------------------------------
-
 tlp.logging.info = function(msg,col)
     -- Check if col is numeric
     if col and not tonumber(col) then
@@ -28,6 +27,15 @@ tlp.logging.info = function(msg,col)
     gFunc.Echo(colour, "[Info] "..msg)
 end
 
+-------------------------------------------------------------------------------
+-- tlp.logging.error
+-- @param msg string The Message you want to display. This will be prefixed with "[Error]"
+-- @param col number The number assignment for the colour you want to display. Use tlp.utils.colourTest() to display all colours up to 255
+--
+-- Example usage:
+-- tlp.logging.error("The Shadow Lords butt was not found to kick!", 5) -- Displays "[Error] The Shadow Lords butt was not found to kick!" in colour 5.
+-- tlp.logging.error("Default colour message") -- Displays "[Error] Default colour message" in colour 68.
+-------------------------------------------------------------------------------
 tlp.logging.error = function(msg,col)
     if col and not tonumber(col) then
         tlp.logging.error("[Error] Colour value must be a number. Defaulting to colour 68.")
@@ -38,10 +46,19 @@ tlp.logging.error = function(msg,col)
     gFunc.Echo(colour, "[Error] "..msg)
 end
 
+-------------------------------------------------------------------------------
+-- tlp.logging.debug
+-- @param msg string The Message you want to display. This will be prefixed with "[Debug]". This will only trigger if you have debug mode enabled (`/lac debug` in-game)
+-- @param col number The number assignment for the colour you want to display. Use tlp.utils.colourTest() to display all colours up to 255
+--
+-- Example usage:
+-- tlp.logging.debug("Looking for the Shadow Lord!", 5) -- Displays "[Debug] Looking for the Shadow Lord!" in colour 5.
+-- tlp.logging.debug("Default colour message") -- Displays "[Debug] Default colour message" in colour 68.
+-------------------------------------------------------------------------------
 tlp.logging.debug = function(msg,col)
     if tlp.helpers.debugEnabled() then
         if col and not tonumber(col) then
-            tlp.logging.error("[Error] Colour value must be a number. Defaulting to colour 3.")
+            tlp.logging.error("[Debug] Colour value must be a number. Defaulting to colour 3.")
             col = 3 -- Default to a safe value
         end
 
