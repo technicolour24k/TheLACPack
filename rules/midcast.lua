3b4f590aa8228e27442d9f19b3894f867490ec23
technicolour24k <hitetsu@hotmail.com>
2025-01-05 14:40:36 +0000

    Update midcast functionality

--- rules/midcast.lua
+++ rules/midcast.lua
@@ -7,23 +7,28 @@ tlp.xi = tlp.xi or {}
 tlp.xi.rules = tlp.xi.rules or {}
 tlp.xi.rules.midcast = tlp.xi.rules.midcast or {}
 
-tlp.xi.rules.midcast.spell =  function (sets, spell)
-    if (sets[spell]) then
-        gFunc.EquipSet(sets[spell])
-    else
-        tlp.logging.debug(string.format("Set not found: sets[%s]",spell))
-    end
-end
+tlp.xi.rules.midcast = {
+    spell = function(sets, spell)
+        if sets[spell] then
+            gFunc.EquipSet(sets[spell])
+        else
+            tlp.logging.debug(string.format("Set not found: sets[%s]", spell))
+        end
+    end,
 
-tlp.xi.rules.midcast.skill = function (sets, skill)
-    if (sets[skill]) then
-        gFunc.EquipSet(sets[skill])
-    else
-        tlp.logging.debug(string.format("Set not found: sets[%s]",skill))
-    end
-end
+    skill = function(sets, skill)
+        if sets[skill] then
+            gFunc.EquipSet(sets[skill])
+        else
+            tlp.logging.debug(string.format("Set not found: sets[%s]", skill))
+        end
+    end,
+}
 
-tlp.xi.rules.midcast = function (sets,spell,skill)
-    tlp.xi.rules.midcast.skill(sets,skill)
-    tlp.xi.rules.midcast.skill(sets,spell)
-end
+-- Set the table to behave like a function
+setmetatable(tlp.xi.rules.midcast, {
+    __call = function(self, sets, spell, skill)
+        self.skill(sets, skill)
+        self.spell(sets, spell)
+    end
+})
